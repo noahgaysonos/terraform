7cee91d5e4dba37b325e3b43c70e4a517305b269
noahgaysonos <noah.gay@sonos.com>
2017-02-18 10:48:29 -0800

    Update main.tf

--- Examples/VPC2/main.tf
+++ Examples/VPC2/main.tf
@@ -1,9 +1,9 @@
 provider "aws" { region = "${var.aws_region}" }
 module "vpc" {
   source = "git::ssh://git@gitserver/org/my-tf-modules//vpc" // double slash intended. See terraform documentation
-  mymod_region = "${var.aws_region}"
-  mymod_az = "${var.az}"
-  mymod_vpc_cidr = "${var.vpc_cidr}"
-  mymod_private_subnet = "${var.private_subnet}"
-  mymod_public_subnet = "${var.public_subnet}"
+  noah_region = "${var.aws_region}"
+  noah_az = "${var.az}"
+  noah_vpc_cidr = "${var.vpc_cidr}"
+  noah_private_subnet = "${var.private_subnet}"
+  noah_public_subnet = "${var.public_subnet}"
 }
