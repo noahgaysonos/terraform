f98759bf5e16a08bd5d5343eb94192a078f49a0e
noahgaysonos <noah.gay@sonos.com>
2017-02-18 10:58:57 -0800

    Update variables.tf

--- Examples/VPC2/variables.tf
+++ Examples/VPC2/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" { default = "us-west-2" }
 variable "az" { default = "us-west-2a" }
-variable "ami" {}
-variable "vpc_cidr" {}
-variable "private_subnet" {}
+variable "ami" { default = "ami-f173cc91"}
+variable "vpc_cidr" { default = "10.3.33.0/24"}
+variable "private_subnet" { default = "10.3.33.0/26"}
