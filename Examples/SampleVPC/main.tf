dfd4755e2a31cd5e30728b36a51bc363144f5124
noahgaysonos <noah.gay@sonos.com>
2017-02-18 09:36:46 -0800

    Update main.tf

--- Examples/SampleVPC/main.tf
+++ Examples/SampleVPC/main.tf
@@ -1,19 +1,19 @@
-resource "alicloud_vpc" "main" {
+resource "noahcloud_vpc" "main" {
   name = "${var.long_name}"
   cidr_block = "${var.vpc_cidr}"
 }
 
-resource "alicloud_vswitch" "main" {
-  vpc_id = "${alicloud_vpc.main.id}"
+resource "noahcloud_vswitch" "main" {
+  vpc_id = "${noahcloud_vpc.main.id}"
   count = "${length(split(",", var.availability_zones))}"
   cidr_block = "${lookup(var.cidr_blocks, "az${count.index}")}"
   availability_zone = "${var.availability_zones}"
   depends_on = [
-    "alicloud_vpc.main"]
+    "noahcloud_vpc.main"]
 }
 
-resource "alicloud_nat_gateway" "main" {
-  vpc_id = "${alicloud_vpc.main.id}"
+resource "noahcloud_nat_gateway" "main" {
+  vpc_id = "${noahcloud_vpc.main.id}"
   spec = "Small"
   bandwidth_packages = [
     {
@@ -23,5 +23,5 @@ resource "alicloud_nat_gateway" "main" {
     }
   ]
   depends_on = [
-    "alicloud_vswitch.main"]
+    "noahcloud_vswitch.main"]
 }
